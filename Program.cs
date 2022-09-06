e069a12664c3ee073a57e5ce5c46b81318af2018
Nastya Spasskaya <Nastya312217@gmail.com>
2022-09-06 21:24:09 +0700

    Решение задачи 3 к семинару 9

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти сумму элементов от М до N, N и M заданны
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+int summa = 0;
+int SumNumber(int m, int n)
+{
+    if(m < n)
+    {
+       summa = summa + m;
+         SumNumber(m + 1, n);
+       return summa + n;
+    }
+    else if(m > n)
+    {
+        summa = summa +n;
+        SumNumber(m,n+1);
+       return summa + m;
+    }
+    else
+    {
+       return summa;    
+    }
+}
+Console.WriteLine($"Сумма элементов в промежутке от {m} до {n} равна {SumNumber(m,n)}");
